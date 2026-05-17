294af8e56db20ba2ceec7df1caecd1be32b17d42
LeonardoGiacomozzi <leonardogabrielgiacomozzi@gmail.com>
2026-05-17 13:12:24 -0300

    fix sonar

--- FiapOficina.OSService/tests/FiapOficina.OSService.Tests/ServiceOrderControllerTests.cs
+++ FiapOficina.OSService/tests/FiapOficina.OSService.Tests/ServiceOrderControllerTests.cs
@@ -82,4 +82,76 @@ public class ServiceOrderControllerTests
         // Assert
         result.Should().BeOfType<NotFoundResult>();
     }
+
+    [Fact]
+    public void DtoClasses_ShouldGetAndSetPropertiesCorrectly()
+    {
+        // Arrange & Act
+        var client = new ClientDto
+        {
+            Name = "Leonardo",
+            Identifier = "123",
+            Phone = "456",
+            Email = "test@test.com",
+            Address = "Rua Test"
+        };
+
+        var vehicle = new VehicleDto
+        {
+            Brand = "Jeep",
+            Model = "Compass",
+            Year = 2021,
+            Plate = "QHJ9B01",
+            Color = "Grey"
+        };
+
+        var service = new ServiceDto
+        {
+            Name = "Service",
+            Description = "Desc",
+            Value = 10,
+            Quantity = 4
+        };
+
+        var material = new MaterialDto
+        {
+            Name = "Material",
+            Description = "Desc",
+            Brand = "Brand",
+            Value = 2,
+            Quantity = 4
+        };
+
+        var dto = new CreateServiceOrderDto
+        {
+            Client = client,
+            Vehicle = vehicle,
+            Services = new List<ServiceDto> { service },
+            Materials = new List<MaterialDto> { material }
+        };
+
+        // Assert
+        dto.Client.Name.Should().Be("Leonardo");
+        dto.Client.Identifier.Should().Be("123");
+        dto.Client.Phone.Should().Be("456");
+        dto.Client.Email.Should().Be("test@test.com");
+        dto.Client.Address.Should().Be("Rua Test");
+
+        dto.Vehicle.Brand.Should().Be("Jeep");
+        dto.Vehicle.Model.Should().Be("Compass");
+        dto.Vehicle.Year.Should().Be(2021);
+        dto.Vehicle.Plate.Should().Be("QHJ9B01");
+        dto.Vehicle.Color.Should().Be("Grey");
+
+        dto.Services[0].Name.Should().Be("Service");
+        dto.Services[0].Description.Should().Be("Desc");
+        dto.Services[0].Value.Should().Be(10);
+        dto.Services[0].Quantity.Should().Be(4);
+
+        dto.Materials[0].Name.Should().Be("Material");
+        dto.Materials[0].Description.Should().Be("Desc");
+        dto.Materials[0].Brand.Should().Be("Brand");
+        dto.Materials[0].Value.Should().Be(2);
+        dto.Materials[0].Quantity.Should().Be(4);
+    }
 }
